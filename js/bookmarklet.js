a8a93677f255869364a9f8d00078e0b826dec06b
Annie Cain <anniejocain@gmail.com>
2017-04-11 15:07:48 -0700

    beta test bookmarklet

--- js/bookmarklet.js
+++ js/bookmarklet.js
@@ -1,16 +1,19 @@
 (function(){
 
 	// the minimum version of jQuery we want
-		var v = "";
-	if(top.frames["kbox"]) {v = top.frames["kbox"].document.getElementsByTagName('h1')[0].innerHTML;}
+		var v = "",
+		u = "";
+	if(top.frames["kbox"]) {v = top.frames["kbox"].document.getElementsByTagName('h1')[0].innerHTML;
+u = top.frames["kbox"].document.getElementById('edit-title').innerText}
 else {
-v = document.getElementsByTagName('h1')[0].innerHTML;}
+v = document.getElementsByTagName('h1')[0].innerHTML;
+u = document.getElementById('edit-title').innerText}
 var p = "<div id='trayframe' style='position:fixed;z-index:1050;'>\
 	<div id='trayframe_veil'></div>\
-	<iframe src='https://lxphpdev01.pugetsound.edu/signoff/create.php?ticketNumber="+v+"&projectTitle=loose+cannon' onload=\"$('#trayframe iframe').slideDown(500);\">Enable iFrames.</iframe>\
+	<iframe src='https://lxphpdev01.pugetsound.edu/signoff/create.php?ticketNumber="+v+"&projectTitle="+u+"' onload=\"$('#trayframe iframe').slideDown(500);\">Enable iFrames.</iframe>\
 	<style type='text/css'>\
 		#trayframe_veil { display: none; position: fixed; width: 100%; height: 100%; top: 0; left: 0; background-color: rgba(255,255,255,.25); cursor: pointer; z-index: 900; }\
-		#trayframe iframe { display: none; position: fixed; top: 0; left: 0; width: 100%; height:375px; z-index: 999; border:none; margin: 0; }\
+		#trayframe iframe { display: none; position: fixed; top: 0; left: 0; width: 100%; height:397px; z-index: 999; border:none; margin: 0; }\
 	</style>\
 </div>";
 if(top.frames["kbox"]) p = "<body>" + p + "</body>";
@@ -47,7 +50,7 @@ if (window.jQuery === undefined || window.jQuery.fn.jquery < v) {
 				setTimeout("$('#trayframe').remove()", 750);
 			});
 			function receiveMessage(event){
-  				if (event.origin !== "lxphpdev01.pugetsound.edu")
+  				if (event.origin !== "https://lxphpdev01.pugetsound.edu")
     				return;
 				$("#trayframe_veil").fadeOut(750);
 				$("#trayframe iframe").slideUp(500);
